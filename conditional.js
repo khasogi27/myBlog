d5393d05da94b8c4a8ea84306ac74120719a6a81
khasogi27 <khasogi27@live.com>
2019-12-02 13:06:24 +0700

    mencobaExtensionBeautify

---
+++ conditional.js
@@ -0,0 +1,30 @@
+//1. dibawah 0 munculin, diatas 20 munculin
+
+// var angka1 = -1
+
+// if(angka1 < 0 || angka1 > 20){
+//     console.log(angka1)
+// }
+
+//2. dibawah 0 munculin, diatas 20 munculin, munculin juga kalau nilainya ganjil
+
+// var angka2 = 22
+
+// if(angka2 %= 2 || angka1 < 0 || angka1 > 20){
+//     console.log("ganjil")
+// }
+// else{
+//     console.log(angka2)
+// }
+
+//3. jika genap di antara 0-20 munculin message "genap antara 0 dan 20"
+
+// var angka3 = 3
+
+if(angka3 > 0 || angka3 % 2){
+    console.log("genap antara 0 dan 20")
+}
+else{
+    console.log(angka3)
+}
+
---
+++ day6.js
@@ -0,0 +1,9 @@
+var deret = 5;
+var jumlah = 0;
+while (deret > 0) { // Loop akan terus berjalan selama nilai deret masih di atas 0
+  jumlah += deret; // Menambahkan nilai variable jumlah dengan angka deret
+  deret--; // Mengubah nilai deret dengan mengurangi 1
+  console.log('Jumlah saat ini: ' + jumlah)
+}
+
+console.log(jumlah);
