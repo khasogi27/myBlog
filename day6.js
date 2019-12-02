d5a75e3b047be6ce344487c720c04cd2b988097e
khasogi27 <khasogi27@live.com>
2019-12-02 13:14:38 +0700

    menghapus file yang tidak perlu

--- day6.js
+++
@@ -1,9 +0,0 @@
-var deret = 5;
-var jumlah = 0;
-while (deret > 0) { // Loop akan terus berjalan selama nilai deret masih di atas 0
-  jumlah += deret; // Menambahkan nilai variable jumlah dengan angka deret
-  deret--; // Mengubah nilai deret dengan mengurangi 1
-  console.log('Jumlah saat ini: ' + jumlah)
-}
-
-console.log(jumlah);
